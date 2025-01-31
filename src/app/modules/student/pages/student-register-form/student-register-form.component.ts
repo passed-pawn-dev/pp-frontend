a63f2a459b5336b46c7892231563502cfbb59dfb
bartosz-urbanowicz <ubartek9@gmail.com>
2025-01-31 20:06:03 +0100

    Add student register form

---
+++ src/app/modules/student/pages/student-register-form/student-register-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-register-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-register-form.component.html',
+  styleUrl: './student-register-form.component.scss'
+})
+export class StudentRegisterFormComponent {}
