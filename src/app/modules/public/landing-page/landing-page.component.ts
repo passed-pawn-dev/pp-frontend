c80cd1ad565d7af167c5e75d25fb90306fccb6a6
bartosz-urbanowicz <ubartek9@gmail.com>
2025-02-04 21:39:10 +0100

    Add styling and landing page redirects

--- src/app/modules/public/landing-page/landing-page.component.ts
+++ src/app/modules/public/landing-page/landing-page.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
-import { PuzzleChessboardComponent } from '../../shared/components/puzzle-chessboard/puzzle-chessboard.component';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
+import { ButtonModule } from 'primeng/button';
+import { JwtDecoded } from '../../student/models/JwtDecoded';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [PuzzleChessboardComponent, PuzzleChessboardComponent, RouterLink],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
-  public constructor(private readonly authService: AuthService) {}
+export class LandingPageComponent implements OnInit {
+  public constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
 
   protected logIn(): void {
     this.authService.redirectToLoginPage();
+  }
+
+  public async ngOnInit(): Promise<void> {
+    const token = await this.authService.getToken();
+    if (token && this.authService.isLoggedIn()) {
+      const decoded: JwtDecoded = jwtDecode(token);
+      const roles: string[] | undefined = decoded.resource_access['api-client']?.roles;
+      if (roles?.includes('student')) {
+        this.router.navigate(['/student']);
+      } else if (roles?.includes('coach')) {
+        this.router.navigate(['/coach']);
+      }
+    }
   }
 }
