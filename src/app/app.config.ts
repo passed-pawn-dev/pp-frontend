6545fe9f157e37327e95a1ce2fbe1cf839cb3251
bartosz-urbanowicz <ubartek9@gmail.com>
2025-03-21 13:22:31 +0100

    Add automatic token refreshing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,55 @@
-import {
-  APP_INITIALIZER,
-  ApplicationConfig, importProvidersFrom,
-  provideZoneChangeDetection
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { KeycloakService } from 'keycloak-angular';
-import { initializeKeycloak } from './keycloak-init';
+import {
+  AutoRefreshTokenService,
+  UserActivityService,
+  provideKeycloak,
+  withAutoRefreshToken
+} from 'keycloak-angular';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token.interceptor';
-import {MessageService} from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+import { environment } from '../../environment/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark-theme'
+        }
+      },
+      ripple: false
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService]
-    },
-    KeycloakService,
+    provideKeycloak({
+      config: {
+        url: environment.keycloak.authority,
+        realm: environment.keycloak.realm,
+        clientId: environment.keycloak.clientId
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+        checkLoginIframe: false,
+        redirectUri: environment.keycloak.redirectUri
+      },
+      features: [
+        withAutoRefreshToken({
+          onInactivityTimeout: 'none'
+        })
+      ],
+      providers: [AutoRefreshTokenService, UserActivityService]
+    }),
     provideHttpClient(withInterceptors([tokenInterceptor])),
     MessageService
   ]
