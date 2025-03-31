087b9a60f3dea0ac92a6df15d024b342ad95cabc
tomek1421 <t.wojciechowski.648@studms.ug.edu.pl>
2025-03-31 22:34:15 +0200

    Style and upgrade rating component

--- src/app/modules/shared/components/star-rating/star-rating.component.ts
+++ src/app/modules/shared/components/star-rating/star-rating.component.ts
@@ -8,13 +8,29 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './star-rating.component.scss'
 })
 export class StarRatingComponent implements OnInit {
-  @Input() public rating!: number;
+  @Input() public rating: number = 0;
   @Input() public ratingsNumber!: number;
   @Input() public showMinimal: boolean = false;
 
   protected starsArray: null[] = [];
+  protected addHalfStar: Boolean = false;
+  protected emptyStarArray: null[] = [];
 
   public ngOnInit(): void {
-    this.starsArray = Array(this.rating).fill(null);
+    if (Number.isInteger(this.rating)) {
+      this.starsArray = Array(this.rating).fill(null);
+    } else {
+      this.starsArray = Array(Math.floor(this.rating)).fill(null);
+
+      this.addHalfStar = this.rating - Math.floor(this.rating) >= 0.5;
+    }
+
+    if (5 - Math.floor(this.rating) > 0) {
+      if (this.rating - Math.floor(this.rating) >= 0.5) {
+        this.emptyStarArray = Array(5 - Math.floor(this.rating) - 1).fill(null);
+      } else {
+        this.emptyStarArray = Array(5 - Math.floor(this.rating)).fill(null);
+      }
+    }
   }
 }
