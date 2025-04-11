b2f440e3bea207b1ed6cd104112ee6c0f9673f24
bartosz-urbanowicz <ubartek9@gmail.com>
2025-04-11 20:08:29 +0200

    Move quiz to shared component to allow preview

--- src/app/modules/student/pages/student-solve-quiz/student-solve-quiz.component.ts
+++ src/app/modules/student/pages/student-solve-quiz/student-solve-quiz.component.ts
@@ -6,65 +6,15 @@ import { Color, MoveType } from '../../../../chess-logic/models';
 import { Bishop } from '../../../../chess-logic/pieces/bishop';
 import { Queen } from '../../../../chess-logic/pieces/queen';
 import { Knight } from '../../../../chess-logic/pieces/knight';
+import { QuizComponent } from '../../../shared/components/quiz/quiz.component';
 
 @Component({
   selector: 'app-student-solve-quiz',
-  imports: [DisplayChessboardComponent],
+  imports: [QuizComponent],
   templateUrl: './student-solve-quiz.component.html',
   styleUrl: './student-solve-quiz.component.scss'
 })
 export class StudentSolveQuizComponent {
-  // protected quiz = signal<QuizDetails>({
-  //   id: '1',
-  //   title: 'Italian game',
-  //   question: 'What is the best move in this situation?',
-  //   answers: [
-  //     {
-  //       text: 'Pawn to e4',
-  //       newPosition: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1',
-  //       lastMove: {
-  //         piece: new Pawn(Color.White),
-  //         prevSquare: "e2",
-  //         currentSquare: "e4",
-  //         moveType: new Set([MoveType.BasicMove]),
-  //       }
-  //     },
-  //     {
-  //       text: 'Pawn to d4',
-  //       newPosition: 'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1',
-  //       lastMove: {
-  //         piece: new Pawn(Color.White),
-  //         prevSquare: "d2",
-  //         currentSquare: "d4",
-  //         moveType: new Set([MoveType.BasicMove]),
-  //       }
-  //     },
-  //     {
-  //       text: 'Pawn to c4',
-  //       newPosition: 'rnbqkbnr/pppppppp/8/8/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 0 1',
-  //       lastMove: {
-  //         piece: new Pawn(Color.White),
-  //         prevSquare: "c2",
-  //         currentSquare: "c4",
-  //         moveType: new Set([MoveType.BasicMove]),
-  //       }
-  //     },
-  //     {
-  //       text: 'Pawn to b4',
-  //       newPosition: 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR w KQkq - 0 1',
-  //       lastMove: {
-  //         piece: new Pawn(Color.White),
-  //         prevSquare: "b2",
-  //         currentSquare: "b4",
-  //         moveType: new Set([MoveType.BasicMove]),
-  //       }
-  //     }
-  //   ],
-  //   solution: 0,
-  //   hint: 'Attack the center',
-  //   positon: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-  //   explanation: 'In italian game you should start with e4'
-  // });
   protected quiz = signal<QuizDetails>({
     id: '2',
     title: 'Szewczyk',
@@ -121,41 +71,4 @@ export class StudentSolveQuizComponent {
     explanation:
       "Moving the queen to h4 delivers checkmate because White's weak f2 square is exposed, leading to a quick defeat."
   });
-
-  protected answerSubmitted: boolean = false;
-  protected solved: boolean | undefined;
-  protected hintShown: boolean = false;
-
-  protected selectedAnswer = signal<number | undefined>(undefined);
-  protected boardFen = computed(() =>
-    this.selectedAnswer() === undefined
-      ? this.quiz().positon!
-      : this.quiz().answers[this.selectedAnswer()!].newPosition!
-  );
-
-  protected lastMove = computed(() =>
-    this.selectedAnswer() === undefined
-      ? undefined
-      : this.quiz().answers[this.selectedAnswer()!].lastMove
-  );
-
-  protected selectAnswer(index: number): void {
-    this.selectedAnswer.set(index);
-  }
-
-  protected showHint(): void {
-    this.hintShown = true;
-  }
-
-  protected reset(): void {
-    this.hintShown = false;
-    this.selectedAnswer.set(undefined);
-    this.answerSubmitted = false;
-    this.solved = undefined;
-  }
-
-  protected submitAnswer(): void {
-    this.answerSubmitted = true;
-    this.solved = this.selectedAnswer() === this.quiz().solution;
-  }
 }
