a9442db0414dc376b8b69dc6c965d8620f95f636
tomek1421 <t.wojciechowski.648@studms.ug.edu.pl>
2025-04-14 21:06:46 +0200

    Refactored student styles

--- src/app/modules/coach/models/LessonDetails.ts
+++ src/app/modules/coach/models/LessonDetails.ts
@@ -1,11 +1,13 @@
 import { Example } from '../../student/models/Example';
+import { Quiz } from '../../student/models/Quiz';
 import { Video } from '../../student/models/Video';
 import { Exercise } from './Exercise';
 
 export interface LessonDetails {
   id: string;
   lessonNumber: number;
-  video: Video;
+  videos: Video[];
   exercises: Exercise[];
   examples: Example[];
+  quizzes: Quiz[];
 }
