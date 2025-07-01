9460f2a1796b5b26b1efb4fcdb3745d387982efa
bartosz-urbanowicz <ubartek9@gmail.com>
2025-07-01 08:32:31 +0200

    Add role to ai requests, minor fixes

--- src/app/modules/shared/services/ai-chat.service.ts
+++ src/app/modules/shared/services/ai-chat.service.ts
@@ -1,16 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Answer } from '../models/answer.model';
+import { AuthService } from '../../core/services/auth.service';
+import { JwtDecoded } from '../models/jwt-decoded.model';
+import { jwtDecode } from 'jwt-decode';
+import { environment } from '../../../../../environment/environment.fullstack-local';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AiChatService {
   private httpClient = inject(HttpClient);
+  private authService = inject(AuthService);
+
   public askQuestion(question: string): Observable<Answer> {
-    return this.httpClient.post<Answer>('/ai/ask', {
-      question: question
-    });
+    let role = 'unregistered';
+    const token = this.authService.getToken();
+
+    if (token !== undefined) {
+      const decoded: JwtDecoded = jwtDecode(token);
+      const roles: string[] | undefined =
+        decoded.resource_access[environment.keycloak.apiClientId].roles;
+
+      if (roles && roles.includes('student')) {
+        role = 'student';
+      } else {
+        role = 'coach';
+      }
+    }
+
+    const params = new HttpParams().set('role', role);
+
+    return this.httpClient.post<Answer>(
+      '/ai/ask',
+      {
+        question: question
+      },
+      { params: params }
+    );
   }
 }
--- src/app/modules/student/pages/student-my-courses/student-my-courses.component.ts
+++ src/app/modules/student/pages/student-my-courses/student-my-courses.component.ts
@@ -4,11 +4,12 @@ import { MyCourse } from '../../models/my-course.model';
 import { StudentCourseService } from '../../services/student-course.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { StudentMyCourseTileComponent } from '../../components/student-my-course-tile/student-my-course-tile.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-student-my-courses',
   standalone: true,
-  imports: [StudentMyCourseTileComponent],
+  imports: [StudentMyCourseTileComponent, RouterLink],
   templateUrl: './student-my-courses.component.html',
   styleUrl: './student-my-courses.component.scss'
 })
